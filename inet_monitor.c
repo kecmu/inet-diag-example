bb92506b7c8f474a344fbd9290cfafbf8698eaf8
Ke Wang US1072104 <wangke@wangke-m01.vmware.com>
2017-08-16 16:35:22 -0700

    tcp monitoring

--- inet_monitor.c
+++ inet_monitor.c
@@ -247,8 +247,8 @@ int parse_diag_msg(struct inet_diag_msg *diag_msg, int rtalen){
                         tcpi->tcpi_unacked,
                         tcpi->tcpi_snd_cwnd);*/
 
-                /*fprintf(stdout, "State: %s, last receive: %u \n", tcp_states_map[tcpi->tcpi_state],
-                        tcpi->tcpi_last_data_recv);*/
+                fprintf(stdout, "State: %s, last receive: %u \n", tcp_states_map[tcpi->tcpi_state],
+                        tcpi->tcpi_last_data_recv);
                 if(tcpi->tcpi_last_data_recv < 100)
                     active_conn += 1;
 
