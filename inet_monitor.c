e6f7748f6b8848a0e2bb1f030e9add7bc497408b
Ke Wang US1072104 <wangke@wangke-m01.vmware.com>
2017-08-16 16:01:26 -0700

    tcp monitoring

--- inet_monitor.c
+++ inet_monitor.c
@@ -299,6 +299,7 @@ int main(int argc, char *argv[]){
             diag_msg = (struct inet_diag_msg*) NLMSG_DATA(nlh);
             rtalen = nlh->nlmsg_len - NLMSG_LENGTH(sizeof(*diag_msg));
             int tmp = parse_diag_msg(diag_msg, rtalen);
+            fprintf(stdout, "return result: %u\n", tmp);
             if(tmp >= 0)
                 current_conn += tmp;
 
