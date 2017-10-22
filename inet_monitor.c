69308ceb649c3cb55e46a7c510171a234c23f7b8
kecmu <klwangke@gmail.com>
2017-10-22 00:19:16 -0400

    Update inet_monitor.c

--- inet_monitor.c
+++ inet_monitor.c
@@ -131,7 +131,7 @@ int send_diag_msg(int sockfd, int port_num){
 
     //Filter out some states, to show how it is done
     conn_req.idiag_states = TCPF_ALL &
-        ~((1<<TCP_SYN_RECV) | (1<<TCP_TIME_WAIT) | (1<<TCP_CLOSE) | (1<<TCP_LISTEN));
+        ~((1<<TCP_SYN_RECV) | (1<<TCP_TIME_WAIT) | (1<<TCP_CLOSE));
     //conn_req.idiag_states = TCP_ESTABLISHED;
 
     //Request extended TCP information (it is the tcp_info struct)
